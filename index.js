ddf0d0e9c9d949c86a8b9971ee96d972cea7b663
Gaurav Singh <83450530+Gaurav07Singh@users.noreply.github.com>
2023-04-08 18:25:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to my PC :)");
+const fs = require('fs');
+
+fs.writeFile('read.txt' , "today is awesome day") ,
+(err) => {
+    console.log("files are created");
+    console.log(err);
+} 
+
+fs.addendFile("read.txt" , "new text added"),
+(err) => {
+    console.log('task completed');
+};
+
